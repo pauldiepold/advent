aa5e0bb49684f4455c62ebe22a74e304ea078ae0
Paul Diepold <paul.diepold@yahoo.de>
2020-12-04 10:58:10 +0100

    day2 finished

--- day3.py
+++ day3.py
@@ -11,15 +11,13 @@ trees = list(map(processInput, trees))
 slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 
 result = 1
-height = len(trees)
-width = len(trees[0])
 
 for slope in slopes:
     counter = 0
-    for i in range(height):
-        if i % slope[1] == 0:
-            if trees[i][(slope[0] * i) % width]:
-                counter += 1
+    for i in range(len(trees))[0::slope[1]]:
+        if trees[i][(slope[0] * i) % len(trees[0])]:
+            counter += 1
+    print(counter)
     result = result * counter
 
 print(result)
