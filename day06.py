2c98211dfa0860721f51b7ddf8ce6bf35396e52b
Paul Diepold <paul.diepold@yahoo.de>
2020-12-06 10:53:09 +0100

    refactor day06

--- day06.py
+++ day06.py
@@ -1,10 +1,12 @@
 with open('./data/day06.txt') as file:
-    groups = [set(group.replace('\n', '')) for group in file.read().split('\n\n')]
-
-print('Teil 1:', sum(len(group) for group in groups))
+    groups = file.read().split('\n\n')
 
 
-with open('./data/day06.txt') as file:
-    groups = [set.intersection(*[set(person) for person in group.split('\n')]) for group in file.read().split('\n\n')]
+distinct_answers = [set(group.replace('\n', '')) for group in groups]
 
-print('Teil 2:', sum(len(group) for group in groups))
+print('Teil 1:', sum(len(group) for group in distinct_answers))
+
+
+intersected_answers = [set.intersection(*[set(person) for person in group.split('\n')]) for group in groups]
+
+print('Teil 2:', sum(len(group) for group in intersected_answers))
