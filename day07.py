1874405684d6ac84a9844f7a6e25d0391ebfd38a
Paul Diepold <paul.diepold@yahoo.de>
2020-12-07 19:44:52 +0100

    refactor day07

--- day07.py
+++ day07.py
@@ -1,3 +1,11 @@
+def process_data(contains):
+    if contains == 'no other bags.':
+        return []
+    else:
+        return [[color[2:], int(color[0])] for color in
+                contains.replace('.', '').replace(' bags', '').replace(' bag', '').split(', ')]
+
+
 def find(color, bags):
     filtered = [k for k, v in rules.items() if any(color in i for i in v)]
     for filtered_color in filtered:
@@ -14,17 +22,8 @@ def count(color, bags, f):
 
 
 with open('./data/day07.txt') as file:
-    lines = [rule for rule in file.read().split('\n')]
-
-rules = {}
-for rule in lines:
-    color, contains = rule.split(' bags contain ')
-    if contains == 'no other bags.':
-        contains = []
-    else:
-        contains = [[color[2:], int(color[0])] for color in
-                    contains.replace('.', '').replace(' bags', '').replace(' bag', '').split(', ')]
-    rules[color] = contains
+    lines = [rule.split(' bags contain ') for rule in file.read().split('\n')]
+    rules = {color: process_data(contains) for color, contains in lines}
 
 print('Teil 1:', len(find('shiny gold', set())))
 print('Teil 2:', count('shiny gold', 0, 1))
