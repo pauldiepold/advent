09a02db9d53c21fa853f549f2d63a37c3c290626
Paul Diepold <paul.diepold@yahoo.de>
2020-12-10 14:27:59 +0100

    refactored day10

--- day10.py
+++ day10.py
@@ -3,11 +3,12 @@ from collections import Counter
 with open('./data/day10.txt') as file:
     numbers = [int(number) for number in file.read().split('\n')]
 
-numbers.insert(0, 0)
+numbers.append(0)
 numbers.append(max(numbers) + 3)
 numbers.sort()
 diffs = [numbers[i] - numbers[i - 1] for i in range(1, len(numbers))]
 count = Counter(diffs)
+
 print('Teil 1:', count[1] * count[3])
 
 
@@ -16,20 +17,16 @@ def combinations(ones):
     if ones <= 1:
         return 1
     elif ones > 1:
-        return 2**n - sum([2**i for i in range(0, n - 3 + 1)])
+        return 2 ** n - sum([2 ** i for i in range(0, n - 3 + 1)])
 
 
-current = 0
+ones = 0
 result = 1
-for diff in diffs:
+for idx, diff in enumerate(diffs):
     if diff == 1:
-        current += 1
-    elif diff == 3:
-        result *= combinations(current)
-        current = 0
-    else:
-        print('error')
-if current != 0:
-    result *= combinations(current)
+        ones += 1
+    elif diff == 3 or idx == len(diffs) - 1:
+        result *= combinations(ones)
+        ones = 0
 
 print('Teil 2:', result)
