c5cb056f8c5f41798aaf68c89bd7edc885c6be0f
Paul Diepold <paul.diepold@yahoo.de>
2020-12-15 13:47:27 +0100

    first try day 14-1

---
+++ day14.py
@@ -0,0 +1,28 @@
+def bitmask(input, mask):
+    input = list('{0:032b}'.format(input))
+    print("".join(input), int("".join(input), 2))
+    print(mask)
+    for idx, bit in enumerate(mask):
+        if bit != 'X':
+            input[idx] = bit
+    print("".join(input), int("".join(input), 2))
+    print('')
+    return int("".join(input), 2)
+
+
+with open('./data/day14.txt') as file:
+    lines = file.read().split('\n')
+
+memory = {}
+for line in lines:
+    if line[:4] == 'mask':
+        mask = line[-32:]
+    elif line[:3] == 'mem':
+        current = [int(c) for c in line.replace('mem[', '').replace(']', '').replace('= ', '').split(' ')]
+        memory[current[0]] = bitmask(current[1], mask)
+
+result = 0
+for item in memory.values():
+    result += item
+
+print('Teil 1:', result)
