440971b8f5b1064fc982e7ffee2d4755a95d5470
Paul Diepold <paul.diepold@yahoo.de>
2021-12-05 13:32:25 +0100

    finished day 5 part 2

--- 2021/day05.py
+++ 2021/day05.py
@@ -3,16 +3,23 @@ import numpy as np
 with open('data/day05.txt') as file:
     lines = np.array([[[int(c) for c in y.split(',')] for y in line.split(' -> ')] for line in file.read().split('\n')])
 
-coords = np.zeros((np.max(lines[:, :, 0]), np.max(lines[:, :, 1])))
+coords1 = np.zeros((np.max(lines[:, :, 0]) + 1, np.max(lines[:, :, 1]) + 1))
+coords2 = np.copy(coords1)
 
 for line in lines:
-    x1, y1 = line[0]
-    x2, y2 = line[1]
+    x1, y1, x2, y2 = line.flatten()
+
     if x1 == x2 or y1 == y2:
         x_min = min(x1, x2)
         x_max = max(x1, x2)
         y_min = min(y1, y2)
         y_max = max(y1, y2)
-        coords[x_min:x_max + 1, y_min:y_max + 1] += 1
 
-print(f'Part 1:  {np.count_nonzero(coords > 1)}')
+        coords1[x_min:x_max + 1, y_min:y_max + 1] += 1
+
+    dx, dy = np.sign([x2 - x1, y2 - y1])
+    for n in range(max(abs(x2 - x1), abs(y2 - y1)) + 1):
+        coords2[x1 + n * dx, y1 + n * dy] += 1
+
+print(f'Part 1:  {np.count_nonzero(coords1 > 1)}')
+print(f'Part 2:  {np.count_nonzero(coords2 > 1)}')
