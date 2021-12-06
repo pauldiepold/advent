828a78019fe7925f724a0a53616c1c497d07bd66
Paul Diepold <paul.diepold@yahoo.de>
2021-12-06 12:51:08 +0100

    finished day 6

---
+++ 2021/day06.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def fish(timers, days):
+    for i in range(days):
+        timers = timers[1:7] + [timers[0] + timers[7]] + [timers[8]] + [timers[0]]
+
+
+if __name__ == "__main__":
+    with open('data/day06.txt') as file:
+        input_state = np.bincount(np.array([int(c) for c in file.readline().split(',')]))
+
+    initial_timers = [0] * 10
+    initial_timers[:6] = input_state
+
+    print(f'Part 1: {fish(initial_timers.copy(), 80)}')
+    print(f'Part 2: {fish(initial_timers.copy(), 256)}')
