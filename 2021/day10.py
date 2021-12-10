a0a5af36cda7174c4454a7e26fd062331b2a3627
Paul Diepold <paul.diepold@yahoo.de>
2021-12-10 10:59:54 +0100

    finished day 10

---
+++ 2021/day10.py
@@ -0,0 +1,39 @@
+def part1(data):
+    opening = ['(', '[', '{', '<']
+    match = {'(': ')', '[': ']', '{': '}', '<': '>'}
+    scores1 = {')': 3, ']': 57, '}': 1197, '>': 25137}
+    scores2 = {'(': 1, '[': 2, '{': 3, '<': 4}
+    result1 = 0
+    autocomplete_scores = []
+
+    for line in data:
+        stack = []
+        corrupted = False
+        for c in line:
+            if c in opening:
+                stack.append(c)
+            else:
+                popped = stack.pop()
+                if c != match[popped]:
+                    result1 += scores1[c]
+                    corrupted = True
+
+        if not corrupted:
+            score = 0
+            for c in stack[::-1]:
+                score *= 5
+                score += scores2[c]
+            autocomplete_scores.append(score)
+
+    result2 = sorted(autocomplete_scores)[int((len(autocomplete_scores) - 1) / 2)]
+
+    return result1, result2
+
+
+if __name__ == "__main__":
+    with open('data/day10.txt') as file:
+        lines = [[c for c in line] for line in file.read().split('\n')]
+
+    res1, res2 = part1(lines)
+    print(f'Part 1: {res1}')
+    print(f'Part 2: {res2}')
